89a7845bcaf2daa8b5a69d297541188cf9a6d766
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-03 13:26:45 +0545

    feat: pagination

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -2,6 +2,7 @@ import Search from "@/app/ui/dashboard/search/search"
 import styles from "@/app/ui/dashboard/users/users.module.css"
 import Link from "next/link"
 import Image from "next/image"
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
 const UsersPage = () => {
     return (
         <div className={styles.container}>
@@ -65,6 +66,7 @@ const UsersPage = () => {
                     </tr>
                 </tbody>
             </table>
+            <Pagination />
         </div>
     )
 }
