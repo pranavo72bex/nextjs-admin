8320a69a60a72f4344c501d197ac6a336a6d1c05
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-04 09:54:38 +0545

    feat: product page

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,72 @@
+import styles from "@/app/ui/dashboard/products/products.module.css"
+import Search from "@/app/ui/dashboard/search/search"
+import Link from "next/link"
+import Image from "next/image"
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
 const ProductsPage = () => {
     return (
-        <div>
-            ProductsPage
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for product..." />
+                <Link href="/dashboard/users/add">
+                    <button className={styles.addButton}>
+                        Add New
+                    </button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>
+                            Title
+                        </td>
+                        <td>
+                            Description
+                        </td>
+                        <td>
+                            Price
+                        </td>
+                        <td>
+                            Created at
+                        </td>
+                        <td>
+                            Stock
+                        </td>
+                        <td>
+                            Action
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.product}>
+                                <Image src="/noproduct.jpg" alt="" width={40} height={40} className={styles.productImage} />
+                                Iphone
+                            </div>
+                        </td>
+                        <td>
+                            dfadfadsfadfdfdsafdsfadfafdffgdfgsfgf
+                        </td>
+                        <td>
+                            $123
+                        </td>
+                        <td>Oct 29 2023</td>
+                        <td>34</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href="/">
+                                    <button className={`${styles.button} ${styles.view}`}>
+                                        View
+                                    </button>
+                                </Link>
+                                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination />
         </div>
     )
 }
