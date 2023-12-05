387e15fe989f046039f0390bbc97558e0c39bba4
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-05 13:41:54 +0545

    feat: login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,16 @@
+import styles from "@/app/ui/login/login.module.css"
 const LoginPage = () => {
     return (
-        <div>Login</div>
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+                <h1>Login</h1>
+                <input type="text" placeholder="username" />
+                <input type="password" placeholder="password" />
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
     )
 }
 
