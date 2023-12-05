fc68b472634bf0ea925cc52a5329e22e1e155bbb
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-05 10:41:50 +0545

    feat: single user ui

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -5,11 +5,35 @@ const SingleUserPage = () => {
         <div className={style.container}>
             <div className={style.infoContainer}>
                 <div className={style.imageContainer}>
-                    <Image src="/avatar.png" alt="" fill />
+                    <Image src="/noavatar.png" alt="" fill />
                 </div>
-                John Doe
+                <div className={style.username}>John Doe</div>
             </div>
-            <div className={style.formContainer}></div>
+            <div className={style.formContainer}>
+                <form action="" className={style.form}>
+                    <label> Username</label>
+                    <input type="text" name="username" placeholder="John Doe" />
+                    <label> Email</label>
+                    <input type="email" name="email" placeholder="John@gmail.com" />
+                    <label> Password</label>
+                    <input type="password" name="password" />
+                    <label> Phone</label>
+                    <input type="text" name="username" placeholder="John Doe" />
+                    <label> Address</label>
+                    <textarea type="text" name="address" placeholder="John Doe"></textarea>
+                    <label>Is Admin</label>
+                    <select name="isAdmin" id="isAdmin">
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                    </select>
+                    <label> Is Active</label>
+                    <select name="isActive" id="isActive">
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                    </select>
+                    <button>Update</button>
+                </form>
+            </div>
         </div>
     )
 }
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -55,7 +55,7 @@ const UsersPage = () => {
                         <td>active</td>
                         <td>
                             <div className={styles.buttons}>
-                                <Link href="/">
+                                <Link href="/dasboard/users/test">
                                     <button className={`${styles.button} ${styles.view}`}>
                                         View
                                     </button>
