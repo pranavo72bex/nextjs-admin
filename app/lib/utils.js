8d2bbca33d090dea19eda57fe2d6cbaf9bcf97c1
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-07 18:05:27 +0545

    chore: minor change

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,10 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectToDB = async => {
-    const connection = {}
+export const connectToDB = async () => {
+    const connection = {};
     try {
-        mongoose.connect(process.env.MONGO)
+        const result = await mongoose.connect(process.env.MONGO);
+        connection.result = result;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
-}
+};
