f23ec8fef90ce3f4896244546794b0f59d16fb1d
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-21 16:32:54 +0545

    feat: fetch data using react query

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 "use client"
-import { useQuery, useIsFetching } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
+import style from './page.module.css'
 
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
 export default function Home() {
-  const { data } = useQuery<any>({ queryKey: ["user"], queryFn: () => fetch("/api/user").then(res => res.json()) })
+  const { data: todoData, isLoading, isError, isSuccess } = useQuery<Todo[]>({ queryKey: ["todos"], queryFn: () => fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json()) })
+  if (isLoading) return <main><div className={style.isloading}>Looding...</div></main>
+  if (isError) return <main><div className={style.iserror}>Error happened </div></main>
   return (
     <main>
-      <div>
-
+      <div className={style.todos}>
+        {isSuccess && Array.isArray(todoData) && todoData.map((todo: Todo) => <div key={todo.id} className={style.todo}>{todo.title}</div>)}
       </div>
     </main>
   )
