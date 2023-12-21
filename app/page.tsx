5a56c4c09bcdc8c28e03b73f602c65b66883d2e9
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-12-21 16:44:22 +0545

    chore: featch multiple api

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,17 @@ interface Todo {
 }
 export default function Home() {
   const { data: todoData, isLoading, isError, isSuccess } = useQuery<Todo[]>({ queryKey: ["todos"], queryFn: () => fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json()) })
+  const { data: user, } = useQuery<Todo[]>({ queryKey: ["users"], queryFn: () => fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()) })
+
   if (isLoading) return <main><div className={style.isloading}>Looding...</div></main>
   if (isError) return <main><div className={style.iserror}>Error happened </div></main>
   return (
     <main>
       <div className={style.todos}>
-        {isSuccess && Array.isArray(todoData) && todoData.map((todo: Todo) => <div key={todo.id} className={style.todo}>{todo.title}</div>)}
+        {isSuccess && Array.isArray(todoData) && todoData.slice(0, 5).map((todo: Todo) => <div key={" " + todo.id} className={style.todo}>{todo.title}</div>)}
+      </div>
+      <div className={style.user}>
+        {isSuccess && Array.isArray(user) && user.map((user: any) => <div key={" " + user.id} className={style.todo}>{user.name}</div>)}
       </div>
     </main>
   )
