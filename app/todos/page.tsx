0be2842bd5550d2ba63e61607cdd4240718d59c5
pranavo72bex <pranav072bex@ioepc.edu.np>
2024-01-02 17:20:28 +0545

    chore: populate data

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,12 +1,16 @@
 "use client"
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-
+import style from './page.module.css'
 const Todos = () => {
     const mutation: any = useMutation({
         mutationFn: (newTodo) => {
             return axios.post('http://localhost:3001/todos', newTodo)
         }
+    })
+    const { data: todoData } = useQuery<any>({
+        queryKey: ["todos"], queryFn: () =>
+            fetch("http://localhost:3001/todos").then(res => res.json())
     })
     return (
         <div>
@@ -22,13 +26,27 @@ const Todos = () => {
 
                     <button
                         onClick={() => {
-                            mutation.mutate({ id: new Date(), title: 'Do Laundry' })
+                            mutation.mutate({ id: new Date(), title: 'Test' })
                         }}
                     >
                         Create Todo
                     </button>
                 </>
             )}
+
+            <div className={style.todolist}>
+                {
+                    todoData?.map((todo) => {
+                        return (
+                            <div className={style.todo}>
+                                <div className={style.todoTitle}>{todo.title}</div>
+                                <div className={style.todoDate}>{todo.date}</div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     )
 }
