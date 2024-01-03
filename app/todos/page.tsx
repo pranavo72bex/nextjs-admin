2a1fd065696975e1c05ba4d98eb4bd80dcb0bcbb
pranavo72bex <pranav072bex@ioepc.edu.np>
2024-01-03 09:55:26 +0545

    chore: usequeryclient and mutation

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,17 +1,34 @@
 "use client"
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import style from './page.module.css'
 const Todos = () => {
+    const queryClient = useQueryClient()
     const mutation: any = useMutation({
         mutationFn: (newTodo) => {
             return axios.post('http://localhost:3001/todos', newTodo)
+        },
+        onMutate: (newTodo) => {
+            console.log("A mutation is about to happen")
+        },
+        onError: (error, variables, context) => {
+            console.log("An error happened", error.message)
+        },
+        onSuccess: (data, variables, context) => {
+            console.log("Mutation was successful", data)
+            queryClient.invalidateQueries({ queryKey: ["todos"] });
+        },
+        onSettled: (data, error, variables, context) => {
+            console.log("Mutation has been settled")
         }
+
     })
     const { data: todoData } = useQuery<any>({
         queryKey: ["todos"], queryFn: () =>
             fetch("http://localhost:3001/todos").then(res => res.json())
     })
+
+
     return (
         <div>
             {mutation.isLoading ? (
@@ -38,7 +55,7 @@ const Todos = () => {
                 {
                     todoData?.map((todo) => {
                         return (
-                            <div className={style.todo}>
+                            <div className={style.todo} key={todo.id}>
                                 <div className={style.todoTitle}>{todo.title}</div>
                                 <div className={style.todoDate}>{todo.date}</div>
                             </div>
