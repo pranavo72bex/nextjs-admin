55b0f0dd49d39fb4b32c08328665f8cd30b42d40
pranavo72bex <pranav072bex@ioepc.edu.np>
2024-01-04 12:46:07 +0545

    feat: pushbutton

--- app/battle/page.tsx
+++ app/battle/page.tsx
@@ -2,10 +2,11 @@ import styles from './battle.module.css';
 const CSSBattlePage = () => {
     return (
         <div className={styles.battleground}>
+            {/* simple square */}
             <div className={styles.simplesqure1}>
                 <div className={styles.simplesqure2}></div>
             </div>
-
+            {/* simple carrom */}
             <div className={styles.carrom}>
                 <div className={styles.Row}>
                     <div className={styles.yellowBox}></div>
@@ -14,6 +15,18 @@ const CSSBattlePage = () => {
                 <div className={styles.Row}>
                     <div className={styles.yellowBox}></div>
                     <div className={styles.yellowBox}></div>
+                </div>
+            </div>
+            <div className={styles.pushbuttonbackground}>
+                <div className={styles.pushbuttonrectangle}>
+                    <div className={styles.pushbuttoncircle}>
+                        <div className={styles.pushbutton}>
+                            <div className={styles.yellocirclepushbutton}>
+
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
